dfe3739ce04f41e525b64ab8ce1a3766abbc77eb
Yahyaygmr <yahyaygmr@gmail.com>
2024-09-04 20:35:53 +0300

    created admin area and tested controllers

---
+++ ECommerce.Client/ECommerce.Client.WebUI/Areas/Admin/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Client.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/{controller}/{action}/{id?}")]
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ECommerce.Client/ECommerce.Client.WebUI/Areas/Admin/ViewComponents/AlertifyComponents/AlertifyComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Client.WebUI.Areas.Admin.ViewComponents.AlertifyComponents
+{
+    public class AlertifyComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
