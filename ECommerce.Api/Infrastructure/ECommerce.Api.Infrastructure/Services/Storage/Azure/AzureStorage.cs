5df9b8bf2455b2f3e0f21b66514433ac06dc8450
Yahyaygmr <yahyaygmr@gmail.com>
2024-09-19 08:31:28 +0300

    ..

---
+++ ECommerce.Api/Infrastructure/ECommerce.Api.Infrastructure/Services/Storage/Azure/AzureStorage.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce.Api.Application.Abstraction.Storage.Azure;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Infrastructure.Services.Storage.Azure
+{
+    public class AzureStorage : IAzureStorage
+    {
+        public Task DeleteAsync(string pathOrContainerName, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetFiles(string pathOrContainerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasFile(string pathOrContainerName, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<(string filename, string path)>> UploadAsync(string pathOrContainerName, IFormFileCollection files)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
